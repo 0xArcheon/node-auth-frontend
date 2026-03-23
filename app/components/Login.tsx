fa97c080c65a563c038aaf5af6240875b960cff5
ImAmlan03 <imamlan03@gmail.com>
2026-03-23 11:19:05 +0530

    initial setup and ui design

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,33 +1,58 @@
 "use client"
 import React from 'react'
 import { useState } from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 function Login() {
     const [mode, setMode] = useState('login');
 
     return (
         <main className='tracking-wide font-sans flex h-full w-full'>
-            <div className="left w-1/2 bg-secondary">
+            <div className="left w-1/2">
+                <img src="/images/orng.jfif" className='object-scale-down'></img>
             </div>
             <div className="right w-1/2 flex justify-center items-center flex-col">
-                <div className="tabs flex gap-2 border-2 py-1 px-1 rounded-md text-lg">
+                {/* <div className="tabs flex gap-2 border-2 py-1 px-1 rounded-md text-lg">
                     <div className="login-tab w-1/2 p-0.5 px-6 bg-primary text-background rounded-sm">Login</div>
                     <div className="register-tab w-1/2 p-0.5 px-6 rounded-sm">Register</div>
-                </div>
-                {mode == 'login' ?
-                    <div className='inputs py-8 text-md'>
-                        <div className='flex flex-col'>
-                            <label htmlFor="">Username</label>
-                            <input type='text' className='border-secondary border-2 rounded-sm'></input>
+                </div> */}
+                <Card className='w-1/2 px-4 py-4'>
+                    <CardContent>
+                        <Tabs>
+                            <TabsList variant="line" className="my-4">
+                                <TabsTrigger value="login" className="bg-primary font-normal text-base">Login</TabsTrigger>
+                                <TabsTrigger value="register" className="bg-primary font-normal text-base">Register</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="login" className="flex flex-col gap-4">
+                                <div className='flex flex-col'>
+                                    <label htmlFor="">Username</label>
+                                    <Input type='text' className='rounded-sm'></Input>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label htmlFor="">Password</label>
+                                    <Input type='password' className='rounded-sm'></Input>
+                                </div>
+                                <Button size="lg" className="w-full bg-primary">Login</Button>
+                            </TabsContent>
+                            <TabsContent value="register">
+                                <div className='flex flex-col'>
+                                    <label htmlFor="">Username</label>
+                                    <Input type='text' className='rounded-sm'></Input>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label htmlFor="">Password</label>
+                                    <Input type='password' className='rounded-sm'></Input>
+                                </div>
+                            </TabsContent>
+                        </Tabs>
+                        <div className="forgot py-4">
+                            Forgot password ?
                         </div>
-                        <div className='flex flex-col'>
-                            <label htmlFor="">Password</label>
-                            <input type='password' className='border-secondary border-2 rounded-sm'></input>
-                        </div>
-                    </div>
-                    :
-                    <div>Register</div>
-                }
+                    </CardContent>
+                </Card>
             </div>
         </main>
     )
