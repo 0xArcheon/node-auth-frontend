95720524bc15f7f74607aafeb0b0e9e3bb8bcbda
ImAmlan03 <imamlan03@gmail.com>
2026-03-24 10:16:18 +0530

    style changes

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -5,14 +5,26 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { HugeiconsIcon } from '@hugeicons/react'
+import { ArrowRight01Icon, AddCircleIcon } from '@hugeicons/core-free-icons'
+import Link from 'next/link'
+import Image from 'next/image'
+
 
 function Login() {
     const [mode, setMode] = useState('login');
 
     return (
-        <main className='tracking-wide font-sans flex h-full w-full'>
-            <div className="left w-1/2">
-                <img src="/images/orng.jfif" className='object-scale-down'></img>
+        <main className='tracking-wide font-mono flex h-full w-full'>
+            <div className="left w-1/2 h-full">
+                <div className="relative w-full h-full">
+                    <Image
+                        src="/images/orng.jpg"
+                        alt="orange"
+                        fill
+                        className="object-cover"
+                    />
+                </div>
             </div>
             <div className="right w-1/2 flex justify-center items-center flex-col">
                 {/* <div className="tabs flex gap-2 border-2 py-1 px-1 rounded-md text-lg">
@@ -23,33 +35,61 @@ function Login() {
                     <CardContent>
                         <Tabs>
                             <TabsList variant="line" className="my-4">
-                                <TabsTrigger value="login" className="bg-primary font-normal text-base">Login</TabsTrigger>
-                                <TabsTrigger value="register" className="bg-primary font-normal text-base">Register</TabsTrigger>
+                                <TabsTrigger value="login" className="bg-primary font-bold text-base">Login</TabsTrigger>
+                                <TabsTrigger value="register" className="bg-primary font-bold text-base">Register</TabsTrigger>
                             </TabsList>
                             <TabsContent value="login" className="flex flex-col gap-4">
-                                <div className='flex flex-col'>
-                                    <label htmlFor="">Username</label>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">username</label>
                                     <Input type='text' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
                                 </div>
-                                <div className='flex flex-col'>
-                                    <label htmlFor="">Password</label>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">password</label>
                                     <Input type='password' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
                                 </div>
-                                <Button size="lg" className="w-full bg-primary">Login</Button>
+                                <Button size="lg" className="w-full bg-primary font-bold">
+                                    login
+                                    <HugeiconsIcon icon={ArrowRight01Icon} size={28}
+                                        color="currentColor"
+                                        strokeWidth={3} />
+                                </Button>
                             </TabsContent>
                             <TabsContent value="register">
-                                <div className='flex flex-col'>
-                                    <label htmlFor="">Username</label>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">username</label>
                                     <Input type='text' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
                                 </div>
-                                <div className='flex flex-col'>
-                                    <label htmlFor="">Password</label>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">email</label>
+                                    <Input type='email' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
+                                </div>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">password</label>
                                     <Input type='password' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
                                 </div>
+                                <div className='flex flex-col text-primary'>
+                                    <label htmlFor="">re-enter Password</label>
+                                    <Input type='password' className='rounded-sm'></Input>
+                                    <div className="err hidden"></div>
+                                </div>
+                                <Button size="lg" className="w-full bg-primary font-bold">create account
+                                    <HugeiconsIcon icon={AddCircleIcon} size={24}
+                                        color="currentColor"
+                                        strokeWidth={3} />
+                                </Button>
+
                             </TabsContent>
                         </Tabs>
                         <div className="forgot py-4">
-                            Forgot password ?
+                            <Link href="#" className='text-blue-800'>
+                                <u>Forgot password ?</u>
+                            </Link>
+
                         </div>
                     </CardContent>
                 </Card>
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div className='dash flex h-dvh justify-center items-center text-5xl font-serif'>Dashboard</div>
+    )
+}
+
+export default Dashboard
