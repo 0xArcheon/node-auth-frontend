20d90f915ed4ca51c04b6fed6212f871f5d636d6
ImAmlan03 <imamlan03@gmail.com>
2026-03-30 16:51:35 +0530

    basic validation setup using zod and react-hook-forms

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,15 +1,41 @@
 "use client"
 import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { ArrowRight01Icon, AddCircleIcon } from '@hugeicons/core-free-icons'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type loginInputs = {
+    username: string,
+    password: string,
+}
+
+type resgiterInputs = {
+    username: string,
+    email: string,
+    password: string,
+    password2: string,
+}
+
+const schema = z.object({
+    username: z.string().min(3, "username must be at least 3 characters long"),
+    password: z.string().min(4, "password must be at least 4 characters long"),
+    confirmPass: z.string(),
+    email: z.email().min(4, "must be a valid email"),
+})
 
 function Login() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({ resolver: zodResolver(schema) });
+
+    const onSubmit: SubmitHandler<loginInputs> = (data) => console.log(data)
 
     return (
         <main className='tracking-wide font-mono flex h-full w-full'>
@@ -29,11 +55,9 @@ function Login() {
                     </div>
                 </div>
             </div>
+
             <div className="right w-full md:w-2/3 lg:w-1/2 flex justify-center items-center flex-col">
-                {/* <div className="tabs flex gap-2 border-2 py-1 px-1 rounded-md text-lg">
-                    <div className="login-tab w-1/2 p-0.5 px-6 bg-primary text-background rounded-sm">Login</div>
-                    <div className="register-tab w-1/2 p-0.5 px-6 rounded-sm">Register</div>
-                </div> */}
+
                 <Card className='w-4/5 md:w-2/3 lg:w-1/2 px-4 py-4'>
                     <CardContent>
                         <Tabs>
@@ -42,50 +66,57 @@ function Login() {
                                 <TabsTrigger value="register" className="bg-primary font-bold text-base">Register</TabsTrigger>
                             </TabsList>
                             <TabsContent value="login" className="flex flex-col gap-4">
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">username</label>
-                                    <Input type='text' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">password</label>
-                                    <Input type='password' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <Button size="lg" className="w-full bg-primary font-bold">
-                                    login
-                                    <HugeiconsIcon icon={ArrowRight01Icon} size={28}
-                                        color="currentColor"
-                                        strokeWidth={3} />
-                                </Button>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+
+                                    <div className='flex flex-col text-primary mb-2'>
+                                        <label htmlFor="">username</label>
+                                        <Input type='text' className='rounded-sm' {...register("username")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.username && <p>{errors.username.message}</p>}</div>
+                                    </div>
+                                    <div className='flex flex-col text-primary mb-2'>
+                                        <label htmlFor="">password</label>
+                                        <Input type='password' className='rounded-sm' {...register("password")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.password && <p>{errors.password.message}</p>}</div>
+
+                                    </div>
+                                    <Button size="lg" type='submit' className="w-full bg-primary font-bold">
+                                        login
+                                        <HugeiconsIcon icon={ArrowRight01Icon} size={28}
+                                            color="currentColor"
+                                            strokeWidth={3} />
+                                    </Button>
+
+                                </form>
                             </TabsContent>
                             <TabsContent value="register" className="flex flex-col gap-2">
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">username</label>
-                                    <Input type='text' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">email</label>
-                                    <Input type='email' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">password</label>
-                                    <Input type='password' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <div className='flex flex-col text-primary'>
-                                    <label htmlFor="">re-enter Password</label>
-                                    <Input type='password' className='rounded-sm'></Input>
-                                    <div className="err hidden"></div>
-                                </div>
-                                <Button size="lg" className="w-full bg-primary font-bold">create account
-                                    <HugeiconsIcon icon={AddCircleIcon} size={24}
-                                        color="currentColor"
-                                        strokeWidth={3} />
-                                </Button>
+                                <form onSubmit={handleSubmit(onSubmit)}>
 
+                                    <div className='flex flex-col text-primary'>
+                                        <label htmlFor="">username</label>
+                                        <Input type='text' className='rounded-sm' {...register("username")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.username && <p>{errors.username.message}</p>}</div>
+                                    </div>
+                                    <div className='flex flex-col text-primary'>
+                                        <label htmlFor="">email</label>
+                                        <Input type='email' className='rounded-sm' {...register("email")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.email && <p>{errors.email.message}</p>}</div>
+                                    </div>
+                                    <div className='flex flex-col text-primary'>
+                                        <label htmlFor="">password</label>
+                                        <Input type='password' className='rounded-sm' {...register("password")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.password && <p>{errors.password.message}</p>}</div>
+                                    </div>
+                                    <div className='flex flex-col text-primary'>
+                                        <label htmlFor="">re-enter Password</label>
+                                        <Input type='password' className='rounded-sm' {...register("confirmPass")}></Input>
+                                        <div className="err text-xs text-orange-600 tracking-tighter">{errors.confirmPass && <p>{errors.confirmPass.message}</p>}</div>
+                                    </div>
+                                    <Button size="lg" type="submit" className="w-full bg-primary font-bold">create account
+                                        <HugeiconsIcon icon={AddCircleIcon} size={24}
+                                            color="currentColor"
+                                            strokeWidth={3} />
+                                    </Button>
+                                </form>
                             </TabsContent>
                         </Tabs>
                         <div className="forgot py-4">
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
-"use client"
 import Login from "@/app/components/Login";
-import Image from "next/image";
-import { useQuery } from '@tanstack/react-query';
-
 
 export default function Home() {
-
-  const { isPending, error, data } = useQuery({
-    queryKey: ["testQuery"],
-    queryFn: async () => {
-      const result = await fetch("https://jsonplaceholder.typicode.com/users");
-      return await result.json();
-    }
-  });
-
-  if (isPending) return "Loading..."
-  if (error) return ("An error occured, " + error.message);
-
   return (
-    <main className="h-dvh flex justify-center items-center relative">
+    <div className="h-dvh flex justify-center items-center relative">
       <Login />
-    </main>
+    </div>
   );
 }
