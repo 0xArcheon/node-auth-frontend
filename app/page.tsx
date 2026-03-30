216c3870fde755ec39b2c85b434ca89d6f87b823
ImAmlan03 <imamlan03@gmail.com>
2026-03-30 11:52:04 +0530

    sample tanstack query tested

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+"use client"
 import Login from "@/app/components/Login";
 import Image from "next/image";
+import { useQuery } from '@tanstack/react-query';
+
 
 export default function Home() {
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["testQuery"],
+    queryFn: async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/users");
+      return await result.json();
+    }
+  });
+
+  if (isPending) return "Loading..."
+  if (error) return ("An error occured, " + error.message);
+
   return (
     <main className="h-dvh flex justify-center items-center relative">
       <Login />
-      {/* <div className="absolute inset-0">
-        <Image
-          src="/images/orng.jpg"
-          alt="orange"
-          fill
-          className="object-cover"
-        ></Image>
-      </div> */}
     </main>
   );
 }
